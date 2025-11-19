60bc19292df50c9d27da0d418b8b65e0d2fb3019
bartdebruin-truepeople <bart@truepeople.nl>
2025-11-19 13:10:14 +0100

    switched UI buttons in orga popup

--- src/components/Popup/UpdateOrganizationNodePopupComponent.vue
+++ src/components/Popup/UpdateOrganizationNodePopupComponent.vue
@@ -189,21 +189,6 @@ const deleteNode = () => {
     <template #actions>
       <div class="flex items-center justify-between w-full">
         <div class="flex items-center space-x-4">
-          <button 
-            type="button"
-            @click="saveNode"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Save
-          </button>
-          <button 
-            type="button"
-            @click="closeModal"
-            class="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700"
-          >
-            Cancel
-          </button>
-        </div>
         <button 
           v-if="mode === 'edit'"
           type="button"
@@ -212,6 +197,21 @@ const deleteNode = () => {
         >
           Delete
         </button>
+          <button 
+            type="button"
+            @click="closeModal"
+            class="text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700"
+          >
+            Cancel
+          </button>
+        </div>
+          <button 
+            type="button"
+            @click="saveNode"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Save
+          </button>
       </div>
     </template>
   </BasePopupComponent>
